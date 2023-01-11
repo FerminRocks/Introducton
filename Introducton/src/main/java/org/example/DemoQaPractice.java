9bb66f34b40e57e7abd57e21becfb5876559d056
FerminRocks <yael.fmp@gmail.com>
2023-01-11 15:25:43 -0600

    Update

---
+++ Introducton/src/main/java/org/example/DemoQaPractice.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DemoQaPractice {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://demoqa.com/");
+        driver.
+
+
+    }
+}
